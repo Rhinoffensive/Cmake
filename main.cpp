ebb044be340dfc9ee3269f75add04c70d5f6633a
RTT-ThinkPad <renantaylan@gmail.com>
2021-12-17 12:36:08 +0300

    glfw build and create window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <adder.h>
+#include <GLFW/glfw3.h>
 //#include <boost/python.hpp>
 
 
@@ -8,5 +9,37 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << add(5.0f, 6.0f) << std::endl;
+
+
+    GLFWwindow* window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        //draw();
+
+        // Update animation
+        //animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
